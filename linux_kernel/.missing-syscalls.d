e5b3be9449661919fa70cfec5ed14d7b8c4a51c5
zhangbin <commshare.zhang@gmail.com>
2015-08-03 14:35:51 +0800

    framebuffer console and boot logo , mkimage to make uImage

--- linux_kernel/.missing-syscalls.d
+++ linux_kernel/.missing-syscalls.d
@@ -1,8 +1,8 @@
 -:  \
- /home/zhangbin/zbhome/omx/bsp/ns115/linux_kernel/include/linux/kconfig.h \
+ /home/zhangbin/zbhome/omx/bsp/ns115/testBSP/linux_kernel/include/linux/kconfig.h \
  include/generated/autoconf.h \
- /home/zhangbin/zbhome/omx/bsp/ns115/linux_kernel/arch/arm/include/asm/types.h \
+ /home/zhangbin/zbhome/omx/bsp/ns115/testBSP/linux_kernel/arch/arm/include/asm/types.h \
  include/asm-generic/int-ll64.h \
  arch/arm/include/generated/asm/bitsperlong.h \
  include/asm-generic/bitsperlong.h \
- /home/zhangbin/zbhome/omx/bsp/ns115/linux_kernel/arch/arm/include/asm/unistd.h
+ /home/zhangbin/zbhome/omx/bsp/ns115/testBSP/linux_kernel/arch/arm/include/asm/unistd.h
---
+++ setTools.sh
@@ -0,0 +1,6 @@
+LIARO=/home/zhangbin/android/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin
+#/home/zhangbin/android/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/bin
+MKIMAGE=./mytools/bin
+export PATH=$PATH:$LIARO:$MKIMAGE
+
+echo "PATH IS $PATH"
